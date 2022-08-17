1c01503b052c15c652389a86867ba555958d5d0a
Mustafa <99654337+Typist01@users.noreply.github.com>
2022-08-17 20:04:35 +0100

    part of initial commit

--- backend/src/main/java/com/sparta/habittracker/controllers/UserController.java
+++ backend/src/main/java/com/sparta/habittracker/controllers/UserController.java
@@ -1,9 +1,17 @@
 package com.sparta.habittracker.controllers;
 
+import com.sparta.habittracker.HashingUtility;
+import com.sparta.habittracker.entities.User;
 import com.sparta.habittracker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+//import javax.json.*;
 
 @RestController
 public class UserController {
@@ -12,8 +20,29 @@ public class UserController {
     UserRepository repo;
 
     @GetMapping("/users")
-    public String getUsers(){
-        return "Todo: display users";
+    public ResponseEntity<List<User>> getUsers(){
+        List<User> users = repo.findAll();
+//        JsonObj
+        //        users.stream().filter(e -> new JSONPObject());
+        return ResponseEntity.ok().header("response", "success")
+                .body(repo.findAll());
+    }
+
+    @GetMapping("/user")
+
+
+
+    @PostMapping("/users")
+    ResponseEntity<String> addUser(@RequestBody User user){
+        if(repo.existsByUsername(user.getUsername()) || repo.existsByEmail(user.getEmail())){
+            return ResponseEntity.badRequest().header("response", "fail")
+                    .body("User could not be added, user already exists (check username or email)");
+        }
+        user.setId(("" + user.getUsername() + System.nanoTime()).hashCode());
+        user.setPasswordToken(HashingUtility.hashPassword(user.getPasswordToken()));
+        repo.save(user);
+        return ResponseEntity.ok().header("response", "success")
+                .body("User add successful");
     }
 
 
--- backend/src/main/java/com/sparta/habittracker/repositories/UserRepository.java
+++ backend/src/main/java/com/sparta/habittracker/repositories/UserRepository.java
@@ -4,4 +4,7 @@ import com.sparta.habittracker.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    public boolean existsByUsername(String username);
+    public boolean existsByEmail(String email);
+//    public User findByEmail
 }
