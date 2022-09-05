b7cee0d835c8c0c04f36997ed257a073d2f9058d
ykolarov <kolarov01@protonmail.me>
2022-09-05 10:46:38 +0100

    Save spring error in log file

---
+++ backend/src/main/java/com/sparta/habittracker/LogUtility.java
@@ -0,0 +1,10 @@
+package com.sparta.habittracker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogUtility {
+
+    public static final Logger logger = LoggerFactory.getLogger(HabitTrackerApplication.class);
+
+}
