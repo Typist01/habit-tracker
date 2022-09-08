d6795ebda2ab6544cd63cc9b8c54fe5d4d2d61db
Mustafa <99654337+Typist01@users.noreply.github.com>
2022-09-08 19:33:23 +0100

    tweaked logout button in navbar

--- frontend3/my-app/src/Components/NavBar/NavBar.js
+++ frontend3/my-app/src/Components/NavBar/NavBar.js
@@ -1,17 +1,26 @@
 /** @format */
 
-import "./NavBar.css"
-import WestIcon from '@mui/icons-material/West';
+import "./NavBar.css";
+import WestIcon from "@mui/icons-material/West";
+import { useContext } from "react";
+import { AuthContext } from "../../App";
 
-export default function(){
-    return (
+export default function () {
+  const ctx = useContext(AuthContext);
 
-        <div className="navbar-style">
-            <ul>
-                <li><a href="/">Habits</a></li>
-                <WestIcon/> 
-                <li className="li-logout"><a href="/login">Logout</a> </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="navbar-style">
+      <ul>
+        <li>
+          <a href="/">Habits</a>
+        </li>
+        <WestIcon />
+        <li className="li-logout">
+          <a onClick={ctx.handleLogout} href="/login">
+            {ctx.isLoggedIn ? "Logout" : "Login"}
+          </a>{" "}
+        </li>
+      </ul>
+    </div>
+  );
 }
