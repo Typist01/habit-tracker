b6179177bf5b294a6f7f66a015eaa832497a313c
ykolarov <kolarov01@protonmail.me>
2022-09-08 13:52:04 +0100

    Fix mui icon display issue

--- frontend3/my-app/src/Components/NavBar/NavBar.js
+++ frontend3/my-app/src/Components/NavBar/NavBar.js
@@ -2,16 +2,16 @@
 
 import "./NavBar.css"
 import WestIcon from '@mui/icons-material/West';
-<link
-  rel="stylesheet"
-  href="https://fonts.googleapis.com/icon?family=Material+Icons"
-/>
+
+
+
 export default function(){
     return (
+
         <div className="navbar-style">
             <ul>
                 <li><a href="#">Habits</a></li>
-                <WestIcon/>
+                <WestIcon/> 
                 <li className="li-logout"><a href="#">Logout</a> </li>
             </ul>
         </div>
